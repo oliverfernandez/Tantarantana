34e6cb0d22d8936ed03e496818067f7c1af01d9e
oliverfernandez <oliver.fernandez@marfeel.com>
2018-08-04 15:25:19 +0200

    First commit with basic project architecture: Spring Boot + Maven modules

---
+++ FilmFestWeb/src/main/java/com/tantarantana/festival/TantarantanaFilmFestWebApplication.java
@@ -0,0 +1,12 @@
+package com.tantarantana.festival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TantarantanaFilmFestWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TantarantanaFilmFestWebApplication.class, args);
+	}
+}
---
+++ FilmFestWeb/src/test/java/com/tantarantana/festival/FestivalCineTantarantanaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tantarantana.festival;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FestivalCineTantarantanaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
