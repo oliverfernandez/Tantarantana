a56bdbe5fbc109566bffffdacd891d759c0a6084
oliverfernandez <oliver.fernandez@marfeel.com>
2018-08-12 14:09:51 +0200

    Security architecture

--- FilmFestWeb/src/main/java/com/tantarantana/filmfestweb/intranet/IntranetController.java
+++ FilmFestWeb/src/main/java/com/tantarantana/filmfestweb/intranet/IntranetController.java
@@ -5,31 +5,22 @@ import com.tantarantana.domain.edition.Edition;
 import com.tantarantana.domain.edition.EditionRepository;
 import com.tantarantana.domain.serie.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/intranet")
+@Secured("isAuthenticated()")
 public class IntranetController {
 
-    @Autowired
-    private EditionRepository editionRepository;
-
-    @Autowired
-    private SerieRepository serieRepository;
-
-
-    @GetMapping("/intranet/editions")
-    @ResponseBody
-    public List<Edition> editions() {
-        List<Edition> editions = new ArrayList<>();
-
-        editionRepository.findAll().forEach(editions::add);
-        serieRepository.findAll();
-
-        return editions;
+    @GetMapping({"", "/"})
+    public String index() {
+        return "intranet/index";
     }
 }
